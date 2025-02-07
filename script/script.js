6c3148a378a0d8ef1c9eb5b811b2cc9eb8dfb623
cs50-demon <cs50based@gmail.com>
2025-02-07 12:52:02 -0800

    worked on the header

--- script/script.js
+++ script/script.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     const mobileMenu = document.getElementById("mobile-menu");
     const navLinks = document.querySelector(".menu");
+    const categories = document.querySelectorAll(".categories li");
 
     mobileMenu.addEventListener("click", () => {
+        categories.forEach((category , e) => {
+           category.addEventListener("click", (e) => {
+               console.log(e.target.innerText)
+           });
+        });
         navLinks.classList.toggle("active");
     });
 });
