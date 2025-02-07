609b30c3a37f1ff3808136cf4b6816fbf47fab76
cs50-demon <cs50based@gmail.com>
2025-02-07 11:24:06 -0800

    worked on the header

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const mobileMenu = document.getElementById("mobile-menu");
-    const navLinks = document.querySelector(".nav-links");
+    const navLinks = document.querySelector(".menu");
 
     mobileMenu.addEventListener("click", () => {
         navLinks.classList.toggle("active");
